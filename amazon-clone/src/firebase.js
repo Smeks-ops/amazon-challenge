191901ddbb246809b55defd0bca934eb7fcde7aa
Osemeke Echenim <echenim.osemeke@gmail.com>
2021-08-07 11:30:52 +0100

    project init

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBVBgX-_8M8EEUwhuMp1eDx8dL1_W1JsLI",
+  authDomain: "clone-20b82.firebaseapp.com",
+  projectId: "clone-20b82",
+  storageBucket: "clone-20b82.appspot.com",
+  messagingSenderId: "315984544929",
+  appId: "1:315984544929:web:46dd9170c30cc75df95ce0",
+  measurementId: "G-FZJ8JE86PT"
+};
